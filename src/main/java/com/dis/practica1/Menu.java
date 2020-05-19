22e0bbe7b732c15da511bea64d85c2955b9377bc
param montejano <parammontejano@gmail.com>
2020-05-19 19:38:59 +0200

    clase menu

---
+++ src/main/java/com/dis/practica1/Menu.java
@@ -0,0 +1,17 @@
+package com.dis.practica1;
+
+public class Menu {
+	public static void MenuInicio() {
+		System.out.println("1. Crear xml");
+		System.out.println("2. Cargar xml");
+		System.out.print("Opción: ");
+	}
+	
+	public static void MenuCRUD() {
+		System.out.println("1. Crear producto");
+		System.out.println("2. Crear cliente");
+		System.out.println("3. Crear pedido");
+		System.out.println("4. Salir");
+		System.out.print("Opción: ");
+	}
+}
